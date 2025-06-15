9df5621fffd5314c63ba6938fc021ad232e21eaf
Arshia gh <arshia.galaxy.22@gmail.com>
2025-06-15 04:12:45 +0330

    Delete prime-checker.js

--- prime-checker.js
+++
@@ -1,8 +0,0 @@
-window.isPrimeNumber = num => {
-    if (num <= 1) return `${num} is nothing`;
-    if (num === 2) return `${num} is prime`;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return `${num} is not prime`;
-    }
-    return `${num} is prime`;
-};
